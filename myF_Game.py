6f02c96b597de7cc42ef9ca66ff7ac9b310b1f5b
shaheemgohar <137343942+shaheemgohar@users.noreply.github.com>
2025-08-06 18:52:47 +0800

    turned it into oop

--- myF_Game.py
+++ myF_Game.py
@@ -11,40 +11,55 @@ walkRight = [pygame.image.load(f'R{n}.png') for n in range(1,10)]
 walkLeft = [pygame.image.load(f'L{n}.png') for n in range(1,10)]
 bg = pygame.image.load('bg.jpg')
 char = pygame.image.load('standing.png')
-#x, y
-x = 50 #char spawn  co-ord
-y = 400
-width = 64 #char w
-height = 64 #char h
-vel = 5
-#to jump
-isJump = False
-jumpCount = 10
 
-#var to keep track of which direc is the char moving
-#are they moving and how many steps have they already moved
-left = False
-right = False
-walkCount = 0
+class player(object):
+    def __init__(self,x,y , width, height):
+        self. x= x
+        self. y = y
+        self.width = width
+        self.height = height
+        self.vel = 5
+        self.isJump = False
+        self.jumpCount = 10
+        self.left = False
+        self.right  = False
+        self.walkCount = 0
+        # #x, y
+        # x = 50 #char spawn  co-ord
+        # y = 400
+        # width = 64 #char w
+        # height = 64 #char h
+        # vel = 5
+        # #to jump
+        # isJump = False
+        # jumpCount = 10
+        # #var to keep track of which direc is the char moving
+        # #are they moving and how many steps have they already moved
+        # left = False
+        # right = False
+        # walkCount = 0
+    def draw(self,screen):
+        if self.walkCount + 1 >= 27:
+            self.walkCount = 0
+        if self.left:
+            screen.blit(walkLeft[self.walkCount//3], (self.x,self.y))
+            self.walkCount += 1
+        elif self.right:
+            screen.blit(walkRight[self.walkCount//3], (self.x,self.y))
+            self.walkCount +=1
+        else:
+            screen.blit(char, (self.x,self.y))
 
-def redrawGameScreen():
-    global walkCount
-    screen.blit(bg, (0,0))
-    if walkCount + 1 >= 27:
-        walkCount = 0
-    if left:
-        screen.blit(walkLeft[walkCount//3], (x,y))
-        walkCount += 1
-    elif right:
-        screen.blit(walkRight[walkCount//3], (x,y))
-        walkCount +=1
-    else:
-        screen.blit(char, (x,y))
 
-    pygame.display.update()
+    def redrawGameScreen(self):
+        screen.blit(bg,(0,0))
+        man.draw(screen)
+        pygame.display.update()
 
-run = True
 #main loop
+man = player(300,410,64,64)
+run = True
+
 while run:
     #will get a list of all the events
     #and then we loop through those to check if they have happenend
@@ -57,43 +72,43 @@ while run:
     keys = pygame.key.get_pressed()
     #we check turn by turn
     #if we do it zero then the char will go past the boudary 1xvel
-    if keys[pygame.K_LEFT]and x > vel:
+    if keys[pygame.K_LEFT]and man.x >man.vel:
         #if you go down it adds to the y and if you go right it adds to the x
-        x -= vel
-        left = True
-        right = False
+        man.x -= man.vel
+        man.left = True
+        man.right = False
         #check notes
-    elif keys[pygame.K_RIGHT] and x < screenw - width - vel: 
-        x += vel
-        right = True
-        left = False
+    elif keys[pygame.K_RIGHT] and man.x < screenw - man.width - man.vel: 
+        man.x += man.vel
+        man.right = True
+        man.left = False
     else:
-        right = False
-        left = False
-        walkCount = 0
+        man.right = False
+        man.left = False
+        man.walkCount = 0
 
-    if not (isJump): #we dont let the user move up or down mid air while jumping is true
+    if not (man.isJump): #we dont let the user move up or down mid air while jumping is true
         # if keys[pygame.K_UP] and y > vel:
         #     y -= vel
         # if keys[pygame.K_DOWN] and y < screenh - height - vel:
         #     y += vel
         if keys[pygame.K_SPACE]:
-            isJump = True
-            right = False
-            left = False
-            walkCount = 0
+            man.isJump = True
+            man.right = False
+            man.left = False
+            man.walkCount = 0
     else:
-        if jumpCount >= -10:
+        if man.jumpCount >= -10:
             neg = 1
-            if jumpCount < 0:
+            if man.jumpCount < 0:
                 neg = -1 #so we can start moving downwards when we reach the -ve side of our loop
-            y -= (jumpCount ** 2) * 0.5 * neg#we jump 100 then it subtracts 1 so 90,80
-            jumpCount -= 1
+            man.y -= (man.jumpCount ** 2) * 0.5 * neg#we jump 100 then it subtracts 1 so 90,80
+            man.jumpCount -= 1
         else:
-            isJump = False
-            jumpCount = 10
+            man.isJump = False
+            man.jumpCount = 10
 
 #now we draw rec charac
-    redrawGameScreen()
+    man.redrawGameScreen()
     clk.tick(27)
 pygame.quit()
