e550bfb05738a2505ca73af9fe9796b784d70c27
shaheemgohar <137343942+shaheemgohar@users.noreply.github.com>
2025-08-06 19:32:30 +0800

    added projectile/gun for player

--- myF_Game.py
+++ myF_Game.py
@@ -24,6 +24,7 @@ class player(object):
         self.left = False
         self.right  = False
         self.walkCount = 0
+        self.standing = True
         # #x, y
         # x = 50 #char spawn  co-ord
         # y = 400
@@ -41,23 +42,43 @@ class player(object):
     def draw(self,screen):
         if self.walkCount + 1 >= 27:
             self.walkCount = 0
-        if self.left:
-            screen.blit(walkLeft[self.walkCount//3], (self.x,self.y))
-            self.walkCount += 1
-        elif self.right:
-            screen.blit(walkRight[self.walkCount//3], (self.x,self.y))
-            self.walkCount +=1
-        else:
-            screen.blit(char, (self.x,self.y))
+            #if  moving we move him in that direc
+        if not (self.standing):
 
+            if self.left:
+                screen.blit(walkLeft[self.walkCount//3], (self.x,self.y))
+                self.walkCount += 1
+            elif self.right:
+                screen.blit(walkRight[self.walkCount//3], (self.x,self.y))
+                self.walkCount +=1
+        else: #if not standing still we freeze him facing L or R
+            # screen.blit(char, (self.x,self.y))
+            if self.right:
+                screen.blit(walkRight[0], (self.x, self.y))
+            else:
+                screen.blit(walkLeft[0], (self.x, self.y))
 
-    def redrawGameScreen(self):
+class projectile(object):
+    def __init__(self,x,y,radius, color, facing):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.facing = facing
+        self.vel = 8 * facing 
+    def draw(self,screen):
+        pygame.draw.circle(screen,self.color,(self.x,self.y),self.radius)
+
+def redrawGameScreen():
         screen.blit(bg,(0,0))
         man.draw(screen)
+        for bullet in bullets:
+            bullet.draw(screen)
         pygame.display.update()
 
 #main loop
 man = player(300,410,64,64)
+bullets = []
 run = True
 
 while run:
@@ -68,8 +89,21 @@ while run:
     for e in events:
         if e.type == pygame.QUIT:
             run = False
+    for bullet in bullets:
+        if bullet.x < 500 and bullet.x > 0:
+            bullet.x += bullet.vel
+        else:
+            bullets.pop(bullets.index(bullet))
 #we make a list of all the keys this is to constantly move the charac instead of just one key stroke at a time
     keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_SPACE]:
+        if man.left:
+            facing = -1
+        else:
+            facing = 1
+        if len(bullets) < 5:
+            bullets.append(projectile(round(man.x + man.width // 2), round(man.y+ man.height//2), 6, (0,0,0),facing ))
     #we check turn by turn
     #if we do it zero then the char will go past the boudary 1xvel
     if keys[pygame.K_LEFT]and man.x >man.vel:
@@ -77,14 +111,17 @@ while run:
         man.x -= man.vel
         man.left = True
         man.right = False
+        man.standing = False
         #check notes
     elif keys[pygame.K_RIGHT] and man.x < screenw - man.width - man.vel: 
         man.x += man.vel
         man.right = True
         man.left = False
+        man.standing = False
     else:
-        man.right = False
-        man.left = False
+        man.standing = True
+        # man.right = False
+        # man.left = False
         man.walkCount = 0
 
     if not (man.isJump): #we dont let the user move up or down mid air while jumping is true
@@ -92,7 +129,7 @@ while run:
         #     y -= vel
         # if keys[pygame.K_DOWN] and y < screenh - height - vel:
         #     y += vel
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_UP]:
             man.isJump = True
             man.right = False
             man.left = False
@@ -109,6 +146,6 @@ while run:
             man.jumpCount = 10
 
 #now we draw rec charac
-    man.redrawGameScreen()
+    redrawGameScreen()
     clk.tick(27)
 pygame.quit()
